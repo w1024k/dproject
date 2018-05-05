8eff34b3e2e0d5bdea4be3c2c7a6b315a85d681f
wangyang <w1024k@163.com>
2018-05-05 19:26:48 +0800

    save

--- wangyang/common/middleware/visit.py
+++ wangyang/common/middleware/visit.py
@@ -15,7 +15,6 @@ class VisitCount(object):
         if request.META.has_key('HTTP_X_FORWARDED_FOR'):
 
             forwarded_for_ips = request.META['HTTP_X_FORWARDED_FOR'].split(',')
-            print forwarded_for_ips
             if len(forwarded_for_ips) >= 2:
                 real_ip = forwarded_for_ips[0].strip()
                 if real_ip:
@@ -24,5 +23,4 @@ class VisitCount(object):
                 request.META['REMOTE_ADDR'] = request.META['HTTP_X_FORWARDED_FOR']
 
         ip = request.META['REMOTE_ADDR']
-        print ip,111
         daily_visit_increase.delay(ip=ip, path=path)
