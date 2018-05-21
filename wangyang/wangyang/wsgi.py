8042ed6d6e2cab89b3b3b1474277c79dd8f13fae
ubuntu <w1024k@163.com>
2018-05-21 17:22:28 +0800

    save

--- wangyang/wangyang/wsgi.py
+++ wangyang/wangyang/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "wangyang.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "wangyang.settings_aliyun")
 
 application = get_wsgi_application()
